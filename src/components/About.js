1f354c637d2be5d29b2902c8a6075eb215a4042f
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-02 15:15:26 +0100

    Default styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 export default function About() {
   return (
     <div class="about--section">
-      <h3 class="about--section__title">About</h3>
-      <p class="about--section__text">
+      <h3 class="section--title about--section__title">About</h3>
+      <p class="section--text about--section__text">
         I am a frontend developer with a particular interest in making things
         simple and automating daily tasks. I try to keep up with security and
         best practices, and am always looking for new things to learn.
