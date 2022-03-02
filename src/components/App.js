94f72599f39446e363b183becc5d8fce83f42817
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-02 15:03:41 +0100

    Interests component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Info from './Info';
 import About from './About';
+import Interests from './Interests';
 
 export default function App() {
   return (
     <div class="container">
       <Info />
       <About />
+      <Interests />
     </div>
   );
 }
---
+++ src/components/Interests.js
@@ -0,0 +1,11 @@
+export default function Interests() {
+  return (
+    <div class="interests--section">
+      <h3 class="interests--section__title">Interests</h3>
+      <p class="interests--section__text">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
+}
