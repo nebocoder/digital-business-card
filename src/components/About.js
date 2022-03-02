7f373e22f0ab96814960a40b9a9760be7c6b8fa5
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-02 14:59:45 +0100

    About component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div class="about--section">
+      <h3 class="about--section__title">About</h3>
+      <p class="about--section__text">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </div>
+  );
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Info from './Info';
+import About from './About';
 
 export default function App() {
   return (
     <div class="container">
       <Info />
+      <About />
     </div>
   );
 }
