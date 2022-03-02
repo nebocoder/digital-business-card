8d5293a379866b3a30e8ed2f9e4b4e32f4ac3478
Nemanja <91620216+nebocoder@users.noreply.github.com>
2022-03-02 15:39:32 +0100

    Info section styling

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,17 +5,17 @@ import linkedinIcon from '../images/linkedin.png';
 export default function Info() {
   return (
     <div class="info--section">
-      <img src={portraitImage} />
+      <img class="info--section__image" src={portraitImage} />
       <h1 class="info--section__title">Laura Smith</h1>
       <p class="info--section__subtitle">Frontend Developer</p>
       <small class="info--section__website">laurasmith.website</small>
 
       <div class="info--section__buttons">
-        <button class="info--button-email">
+        <button class="info--button__email">
           <img src={emailIcon} />
           Email
         </button>
-        <button class="info--button-linkedin">
+        <button class="info--button__linkedin">
           <img src={linkedinIcon} />
           Linkedin
         </button>
